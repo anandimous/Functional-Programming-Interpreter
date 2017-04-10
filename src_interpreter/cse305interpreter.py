de8b199c6a689375f916d0467a5c999787b7cbb9
anandimous <anandi@buffalo.edu>
2017-04-09 21:34:23 -0400

    Updated interpreter py file to inc part 2

--- src_interpreter/cse305interpreter.py
+++ src_interpreter/cse305interpreter.py
@@ -23,8 +23,13 @@ def interpreter(infil, outfil):
                 stack.append(line);
             elif line[0] in alpList:
                 stack.append(line[0:len(line)-1] + '\n');
-            elif line[0] == '-':
-                stack.append("expected");
+            elif line[0] is chr(45):
+                if line[1] == '0':
+                    stack.append('0\n');
+                elif is_digit(line[1]) == True:
+                    stack.append(line);
+                else:
+                    stack.append(':error:\n')
             elif '"' in line:
                 line = line.replace('"', '')
                 stack.append(line);
@@ -36,11 +41,11 @@ def interpreter(infil, outfil):
             else:
                 stack.append(':error:\n');
         elif ':true:' in command:
-            stack.append(':true:');
+            stack.append(':true:\n');
         elif ':false:' in command:
-            stack.append(':false:');
+            stack.append(':false:\n');
         elif ':error:' in command:
-            stack.append(':error:');
+            stack.append(':error:\n');
         elif 'add' in command:
             if len(stack)!=0 and len(stack)!=1 and is_digit(stack[len(stack)-2]) and is_digit(stack[len(stack)-1]):
                 stack.append(str( int(stack.pop()) + int(stack.pop()) ) + '\n');
@@ -85,6 +90,49 @@ def interpreter(infil, outfil):
                 stack.append(':error:\n');
         elif 'show' in command:
             print(stack);
+    #                            END OF PART 1                              #
+        elif 'and' in command:
+            if len(stack)!=0 and len(stack)!=1 and is_bool(stack[len(stack)-2]) and is_bool(stack[len(stack)-1]):
+                op2 = stack.pop();
+                op1 = stack.pop();
+                stack.append(str(op1 and op2) + '\n');
+            else:
+                stack.append(':error:\n');
+        elif 'or' in command:
+            if len(stack)!=0 and len(stack)!=1 and is_bool(stack[len(stack)-2]) and is_bool(stack[len(stack)-1]):
+                op2 = stack.pop();
+                op1 = stack.pop();
+                stack.append(str(op1 or op2) + '\n');
+            else:
+                stack.append(':error:\n');
+        elif 'not' in command:
+            if len(stack)!=0 and is_bool(stack[len(stack)-1]):
+                op1 = stack.pop();
+                if op1 == ':true:':
+                    stack.append(':false:\n');
+                else:
+                    stack.append(':true:\n');
+            else:
+                stack.append(':error:\n');
+        elif 'equal' in command:
+            if len(stack)!=0 and len(stack)!=1 and is_digit(stack[len(stack)-2]) and is_digit(stack[len(stack)-1]):
+                op2 = stack.pop();
+                op1 = stack.pop();
+                result = ':true:' if int(op1) == int(op2) else ':false:';
+                stack.append(result + '\n');
+            else:
+                stack.append(':error:\n');
+        elif 'lessThan' in command:
+            if len(stack)!=0 and len(stack)!=1 and is_digit(stack[len(stack)-2]) and is_digit(stack[len(stack)-1]):
+                op2 = stack.pop();
+                op1 = stack.pop();
+                result = ':true:' if int(op1) < int(op2) else ':false:';
+                stack.append(result + '\n');
+            else:
+                stack.append(':error:\n');
+        #elif TBD   TBD     TBD     TBD     TBD     TBD     #
+
+
         elif 'quit' in command:
             while (len(stack)!=0):
                 a = str(stack.pop());
